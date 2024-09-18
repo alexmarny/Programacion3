ec36ec43131e2aa1afc60e7b48b7a3c8d1c8c4be
alexmarny <alexmarnieto@usal.es>
2024-09-18 13:31:26 +0200

    Programa inicial

--- Semana1/holaMundo/src/App.java
+++ Semana1/holaMundo/src/App.java
@@ -1,5 +1,48 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+		Scanner scanRef = new Scanner(System.in);
+		String frase = "Hola, Mundo!";
+		int[] num = new int[3];
+		
+
         System.out.println("Hello, World!");
+		System.out.printf("%S hoy es  %d / %d / %d \n", frase, 18, 9 , 2024);
+		System.out.println("Ingrese su nombre: ");
+		String nombre = scanRef.nextLine();
+		System.out.println("Escriba su edad: ");
+		int edad = scanRef.nextInt();
+
+		System.out.println("su nombre es: " + nombre + " y su edad es: " + edad);
+		System.out.println("A continuacion se le pediran 3 numeros" + "\nSe le dira el mayor y el meno de ellos");
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Ingrese el numero " + (i + 1) + ": ");
+			int entrada = scanRef.nextInt();
+			if (i == 0) {
+				num[i] = entrada;
+			} else if (i == 1) {
+				num[i] = entrada;
+			} else {
+				num[i] = entrada;
+			}
+		}
+		
+		for (int i = 0; i < num.length - 1; i++) {
+			for (int j = 0; j < num.length - i - 1; j++) {
+				if (num[j] > num[j + 1]) {
+					int temp = num[j];
+					num[j] = num[j + 1];
+					num[j + 1] = temp;
+				}
+			}
+		}
+
+		System.out.println("El numero menor es: " + num[0]);
+		System.out.println("El numero mayor es: " + num[num.length - 1]);
+
     }
 }
--- Semana1/ordenarBurbuja/src/App.java
+++ Semana1/ordenarBurbuja/src/App.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
-		int[] numbers = new int[5];
+		int[] lista = new int[5];
 
 		System.out.println("Ingrese 5 números:");
 		for (int i = 0; i < 5; i++) {
-			numbers[i] = scanner.nextInt();
+			lista[i] = scanner.nextInt();
 		}
 
 		// Algoritmo de burbuja
-		for (int i = 0; i < numbers.length - 1; i++) {
-			for (int j = 0; j < numbers.length - 1 - i; j++) {
-				if (numbers[j] > numbers[j + 1]) {
-					int temp = numbers[j];
-					numbers[j] = numbers[j + 1];
-					numbers[j + 1] = temp;
+		for (int i = 0; i < lista.length - 1; i++) {
+			for (int j = 0; j < lista.length - 1 - i; j++) {
+				if (lista[j] > lista[j + 1]) {
+					int temp = lista[j];
+					lista[j] = lista[j + 1];
+					lista[j + 1] = temp;
 				}
 			}
 		}
 
 		System.out.println("Números ordenados:");
-		for (int number : numbers) {
+		for (int number : lista) {
 			System.out.println(number);
 		}
 
