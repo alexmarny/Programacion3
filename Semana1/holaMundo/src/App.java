197fa4bc55635da0f79b871fe93b544b73671bad
alexmarny <145485791+alexmarny@users.noreply.github.com>
2024-09-18 13:35:01 +0200

    App.java

--- Semana1/holaMundo/src/App.java
+++ Semana1/holaMundo/src/App.java
@@ -22,15 +22,10 @@ public class App {
 		for (int i = 0; i < 3; i++) {
 			System.out.println("Ingrese el numero " + (i + 1) + ": ");
 			int entrada = scanRef.nextInt();
-			if (i == 0) {
-				num[i] = entrada;
-			} else if (i == 1) {
-				num[i] = entrada;
-			} else {
-				num[i] = entrada;
-			}
+			num[i] = entrada;
+			
 		}
-		
+		//bubble sort
 		for (int i = 0; i < num.length - 1; i++) {
 			for (int j = 0; j < num.length - i - 1; j++) {
 				if (num[j] > num[j + 1]) {
@@ -41,6 +36,7 @@ public class App {
 			}
 		}
 
+	    	
 		System.out.println("El numero menor es: " + num[0]);
 		System.out.println("El numero mayor es: " + num[num.length - 1]);
 
