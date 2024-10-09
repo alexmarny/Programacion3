4a8858973ceb8797fed197be886add09b42914da
alexmarny <alexmarnieto@usal.es>
2024-10-09 13:31:20 +0200

    Fruteria

---
+++ Semana4/Fruteria/src/App.java
@@ -0,0 +1,14 @@
+import Datos.Producto;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+		System.out.println("Introduzca el precio de las peras hoy");
+
+		
+
+
+		System.out.println("Introduzca el precio de las manzanas hoy");
+
+    }
+}
